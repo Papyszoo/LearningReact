adc4a4607836707e003c94e163b96ea14f29c898
Patryk Kowalczyk <papyszoo@gmail.com>
2021-05-11 23:08:24 +0200

    cra + filtering list

---
+++ testingreact/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import React from 'react';
+import UsersList from './Userlist'
+
+const allUsers = ['Michal', 'Kasia', 'Jacek', 'Marta', 'Tomek', 'Ania'];
+class App extends React.Component {
+  constructor() {
+    super();    
+    
+    this.state = {
+      filteredUsers: allUsers
+    };
+  }
+
+  filterUsers(e) {
+    const text = e.currentTarget.value;
+    const filteredUsers = this.getFilteredUsersForText(text)
+    this.setState({
+      filteredUsers
+    })
+  }
+  
+  getFilteredUsersForText(text) {
+    return allUsers.filter(user => user.toLowerCase().includes(text.toLowerCase()))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input onChange={this.filterUsers.bind(this)}></input>
+        <UsersList users={this.state.filteredUsers}></UsersList>
+      </div>
+    );
+  }
+}
+
+export default App;
