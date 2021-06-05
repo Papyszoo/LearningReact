09326244c4e00d2ebba2537a543f8dd21481791b
Patryk Kowalczyk <papyszoo@gmail.com>
2021-06-06 00:21:30 +0200

    added working redux for contacts list

--- testingreact/src/pages/ContactsListPage/ContactsListPage.js
+++ testingreact/src/pages/ContactsListPage/ContactsListPage.js
@@ -3,16 +3,14 @@ import ContactsList from '../../components/molecules/ContactsList/ContactsList'
 import Navbar from '../../components/organisms/Navbar/Navbar'
 import * as React from "react";
 import { connect } from "react-redux";
-import { contactsFetched } from "./actions";
-export class ContactsListPage extends React.Component {
-  state = {
-    contacts: []
-  };
+import { contactsFetched } from "../../actions";
+
+class ContactsListPage extends React.Component {
 
   componentDidMount() {
     fetch("https://randomuser.me/api/?format=json&results=10")
       .then(res => res.json())
-      .then(json => this.setState({ contacts: json.results }));
+      .then(json => this.props.contactsFetched(json.results));
   }
 
   render() {
@@ -21,7 +19,7 @@ export class ContactsListPage extends React.Component {
         <Navbar></Navbar>
         <main className="ui main text container">
           <ContactsListHeader />
-          { this.state.contacts ? <ContactsList contacts={this.state.contacts} /> : 'Ładowanie'}
+          { this.props.contacts ? <ContactsList contacts={this.props.contacts} /> : 'Ładowanie'}
         </main>
       </div>
     );
@@ -32,5 +30,6 @@ const mapStateToProps = (state) => {
     contacts: state.contacts // (1)
   }
 };
+
 const mapDispatchToProps = { contactsFetched };
-export const AppContainer = connect(mapStateToProps, mapDispatchToProps)(ContactsListPage);
+export default connect(mapStateToProps, mapDispatchToProps)(ContactsListPage);
